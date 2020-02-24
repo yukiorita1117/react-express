4288a277c19872cfa3b71e3673c279ab0436b0fb
折田幸夫 <3204704@gmail.com>
2020-02-24 17:13:04 +0900

    feat params

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = express();
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
-
+//logをcliに表示するmiddleware
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -20,6 +20,13 @@ app.use(cookieParser());
 //毎回ファイル読み込むのroutingするのだるい.固定化する
 app.use(express.static(path.join(__dirname, "public")));
 // app.use(express.static(__dirname + "public")); //上と同じ
+
+//自作middleware(上記全てのmiddlewareが当てはまらないものはここに入る)
+app.use((req, res, next) => {
+  console.log("自作したmiddlewareを表示しています。");
+  //次のmiddleware命令に行きなさいと言う意味
+  next();
+});
 
 // middleware
 app.use("/", indexRouter);
@@ -30,10 +37,29 @@ app.use("/user/:name?", (req, res) => {
     ? res.send("hello, " + req.params.name)
     : res.send("hello, nobody !!");
 });
-//パラメータに正規表現使う
-app.use("/items/:id([0-9]+)", (req, res) => {
-  res.send("your id is " + req.params.id);
+
+//getの前にparamを噛ませてid渡す.第四引数に入ってきた値を設定できる
+app.param("id", (req, res, next, id) => {
+  var users = ["ゆきお", "のぶお", "みつお"];
+  req.params.name = users[id];
+
+  next();
 });
+
+app.get("/items/:id", (req, res) => {
+  //配列にないものが来ると"undefind"が返る。
+  res.send("this account user name is " + req.params.name);
+});
+
+//別routingでもparams共通化
+app.get("/nameTest/:id", (req, res) => {
+  res.send("僕の名前は " + req.params.name);
+});
+
+//パラメータに正規表現使う
+// app.get("/items/:id([0-9]+)", (req, res) => {
+//   res.send("your id is " + req.params.id);
+// });
 
 //内部ファイル読み込み
 app.use("/sample.txt", (req, res) => {
